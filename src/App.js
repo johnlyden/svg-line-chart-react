0d09dc7afb15c5a1bac3ffcb7b943c970c33dc06
johnlyden <john.lyden86@gmail.com>
2017-10-22 14:13:39 -0500

    added function to create fake data for line graph

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  createFakeData(){
+    // This function creates data that doesn't look entirely random
+    const data = []
+    for (let x = 0; x <= 30; x++) {
+      const random = Math.random();
+      const temp = data.length > 0 ? data[data.length-1].y : 50;
+      const y = random >= .45 ? temp + Math.floor(random * 20) : temp - Math.floor(random * 20);
+      data.push({x,y})
+    }
+    // array of objects with x and y coordinates
+    return data;
+  }
+render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        // THIS IS WHERE OUR SVG CHART WILL RENDER
       </div>
     );
   }
 }
-
 export default App;
